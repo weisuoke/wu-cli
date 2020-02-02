288219f733796d501965d481551c1daa8de85b97
梅林的法杖 <530264127@qq.com>
2020-02-02 20:23:49 +0800

    zf cli demo

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,11 @@
 const { version } = require("../package.json");
 
 // 存储模板的位置
-const downloadDirectory =
-  process.env[process.platform === "darwin" ? "HOME" : "USERPROFILE"];
+const downloadDirectory = `${
+  process.env[process.platform === "darwin" ? "HOME" : "USERPROFILE"]
+}/.template`;
 
 module.exports = {
-  version
+  version,
+  downloadDirectory
 };
--- src/create.js
+++ src/create.js
@@ -1,6 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 const axios = require("axios");
 const ora = require("ora");
 const Inquirer = require("inquirer");
+let downloadGitRepo = require("download-git-repo");
+downloadGitRepo = promisify(downloadGitRepo); // 可以把异步的 api 转换成 promise
+const MetalSmith = require("metalsmith"); // 遍历文件夹 找需不需要渲染
+// consolidate 统一了所有的模板引擎
+let { render } = require("consolidate").ejs;
+render = promisify(render);
+let ncp = require("ncp");
+ncp = promisify(ncp);
+const { downloadDirectory } = require("./constants");
 // create 的所有逻辑
 // create 功能是创建项目
 // 拉取你自己的所有项目列出来，让用户选 安装哪个项目
@@ -18,6 +30,16 @@ const fetchTagList = async repo => {
     `https://api.github.com/repos/zhu-cli/${repo}/tags`
   );
   return data;
+};
+const download = async (repo, tag) => {
+  let api = `zhu-cli/${repo}`;
+  if (tag) {
+    api += `#${tag}`;
+  }
+  // /user/xxx/.template/repo
+  const dest = `${downloadDirectory}/${repo}`;
+  await downloadGitRepo(api, dest);
+  return dest; // 下载的最终目录
 };
 
 // 封装 loading 效果
@@ -55,6 +77,61 @@ module.exports = async projectName => {
     choices: tags
   });
 
-  console.log(repo, tag); // 下载模板
-  // 把模板放到一个临时目录里，以备后期使用
+  // 3. 把模板放到一个临时目录里，以备后期使用
+
+  // download-git-repo
+  const result = await waitFnloading(download, "downloading template")(
+    repo,
+    tag
+  );
+
+  // 拿到了下载的目录 直接拷贝当前执行的目录即可 ncp
+  // 把template下的文件，拷贝到执行命令的目录下
+
+  // 如果有 ask.js 文件 // .template/xxx
+  if (!fs.existsSync(path.join(result, "ask.js"))) {
+    // 4. 拷贝
+    // 这个目录 项目名字是否已经存在 如果存在提示当前已经存在
+    await ncp(result, path.resolve(projectName));
+  } else {
+    // 复杂的需要模板渲染，渲染后再拷贝
+    // 把 git 上的项目下载下来，如果有 ask 文件，就是一个复杂的模板，我们需要用户选择，选择后编译模板
+    // metalsmith 只要是编译 都需要这个模块
+
+    // 1. 让用户填写信息
+    await new Promise((resolve, reject) => {
+      MetalSmith(__dirname) // 如果你传入路径，它默认会遍历当前路径下的 src 文件夹
+        .source(result)
+        .destination(path.resolve(projectName))
+        .use(async (files, metal, done) => {
+          const args = require(path.join(result, "ask.js"));
+          let obj = await Inquirer.prompt(args); // 用户填写的结果
+          const meta = metal.metadata();
+          Object.assign(meta, obj);
+          delete files["ask.js"];
+          done();
+        })
+        .use((files, metal, done) => {
+          let obj = metal.metadata();
+          Reflect.ownKeys(files).forEach(async file => {
+            // 这个是要处理的
+            if (file.includes("js") || file.includes("json")) {
+              let content = files[file].contents.toString();
+              if (content.includes("<%")) {
+                content = await render(content, obj);
+                files[file].contents = Buffer.from(content);  // 渲染
+              }
+            }
+          });
+          done();
+        })
+        .build(err => {
+          if (err) {
+            reject();
+          } else {
+            resolve();
+          }
+        });
+    });
+  }
 };
